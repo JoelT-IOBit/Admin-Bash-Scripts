b7a9c7f16432671793991740399288da29a53474
uadm26 <77186949+uadm26@users.noreply.github.com>
2021-03-17 14:48:30 +0000

    updates
    new file created

--- manage_users_4in1.sh
+++ manage_users_4in1.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
-'''
+#!/usr/bin/env bash
+
+: '
 
 The script accepts a username but if the username is not provided will execute the 
 prompt_user function
 
-'''
+'
 
 # Create a prompt_user function
 
@@ -15,14 +16,14 @@ prompt_user () {
         message=${1:-"Enter the account details"}
         echo "$message"
 		# Read the user_name variable from user imput
-        read -p "Enter a username: " user_name
+        read -rp "Enter a username: " user_name
 		# Read the user_password variable from user imput in silent mode
-        read -sp "Enter a password: " user_password
+        read -srp "Enter a password: " user_password
 		# Enter a empty line
         echo ""
 		# Requesting a password confirmation and assigning
 		# the password value to a different value for future validation
-        read -sp "Enter the password again: " user_password_check
+        read -srp "Enter the password again: " user_password_check
         # Enter a empty line
 		echo ""
 }
@@ -59,8 +60,9 @@ create_user () {
 	echo "$user_name created"
 }
 
+# 
 check_file_exists(){
-read -p "Enter the name of the file in CSV format: " file_to_read
+read -rp "Enter the name of the file in CSV format: " file_to_read
 if [ -z "$file_to_read" ]; then
 echo "You need to enter the name of the file - argument cannot be empty"
 return 1
@@ -92,7 +94,7 @@ done < <(tail -n +2 $file_to_read)
 # Create a funtion to delete a user
 delete_user () {
 	# Read the user_name variable from user imput 
-	read -p "Enter user to delete: " user_name
+	read -rp "Enter user to delete: " user_name
 	# Create a while loop to if user exists
 	while  ! check_user "$user_name" ; do
 		echo "User not found"
@@ -104,45 +106,38 @@ delete_user () {
 	echo "$user_name deleted"
 }
 
-while true ; do
-  clear
-  echo "user management"
-  echo "1: Create User"
-  echo "2: Delete User"
-  echo "3: Create users from file"
-  echo "4: Exit"
-  read -sn1 
-  case "$REPLY" in 
-    1) create_user;;
-    2) delete_user;;
-	3) create_user_from_list;;
-    4) exit 0;;
-  esac
-  read -n1 -p "Press any key"
-done
+if [ -z $1 ]; then
 
+	while true ; do
+	  clear
+	  echo ''' User Management Tool:
+	  
+	  4 in 1 tool:
+	  
+	  shell command: ./manage_users.sh <user_name> <user_password>
+	  
+	  Menu:
+	  
+	  Press 1 to Create User
+	  Press 2 to Delete User1
+	  Press 3 to Create users from file
+	  Press 4 to Exit
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	'''
+	read -rsn1 
+	  case "$REPLY" in 
+		1) create_user;;
+		2) delete_user;;
+		3) create_user_from_list;;
+		4) exit 0;;
+	  esac
+	  read -rn1 -p "Press any key"
+	done
+else 
+	user_name=$1
+	user_password=$2
+	sudo useradd -m $user_name && echo "$user_name created"
+	if [ $? = 0 ]; then 
+	check_user $user_name && echo "${user_name}:$user_password" | sudo chpasswd && echo "password set for ${user_name}"
+	fi
+fi
